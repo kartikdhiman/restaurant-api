c596de7901a2b769a3f533d610f059a4bf2bff73
Kartik Dhiman <kartikjuly@gmail.com>
2021-05-01 21:58:22 +0530

    feat: managing identity with Auth0

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { AuthenticationMiddleware } from './common/authentication.middleware';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
 import { ShoppingCartController } from './shopping-cart/shopping-cart.controller';
@@ -8,4 +9,13 @@ import { ShoppingCartController } from './shopping-cart/shopping-cart.controller
   controllers: [ItemsController, ShoppingCartController],
   providers: [ItemsService],
 })
-export class AppModule {}
+export class AppModule {
+  public configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthenticationMiddleware)
+      .forRoutes(
+        { path: '/items', method: RequestMethod.POST },
+        { path: '/shopping-cart', method: RequestMethod.POST },
+      );
+  }
+}
